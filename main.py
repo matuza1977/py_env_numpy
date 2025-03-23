de42491a2d5908e7302def75890e09e4ebc725dd
matuza1977 <matuzzagames@gmail,com>
2025-03-23 06:59:36 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+#Criando um array com numpy
+array = np.array([1, 2, 3, 4, 5])
+print ("Array numpy:", array)
+
+#Criando um DataFrame Pandas
+df = pd.DataFrame({'Coluna A':[10, 20, 30], 'Coluna B':[40, 50, 60]})
+print ("DataFrame Pandas:\n", df)
+
+
